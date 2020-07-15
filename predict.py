c50df219d82a6a153068d5622c93048f6ff0116f
Giwrgos Angelis <56271926+georange7@users.noreply.github.com>
2020-07-15 19:11:44 +0300

    Update predict.py

--- predict.py
+++ predict.py
@@ -10,6 +10,14 @@ from keras.models import load_model
 from tqdm import tqdm
 import numpy as np
 
+config = tf.compat.v1.ConfigProto(
+    gpu_options = tf.compat.v1.GPUOptions(per_process_gpu_memory_fraction=0.9)
+    # device_count = {'GPU': 1}
+)
+config.gpu_options.allow_growth = True
+session = tf.compat.v1.Session(config=config)
+tf.compat.v1.keras.backend.set_session(session)
+
 def _main_(args):
     config_path  = args.conf
     input_path   = args.input
